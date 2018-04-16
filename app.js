68a3236c97fbe23fceb594f23483e3e3fe125d4b
ihoey <mail@ihoey.com>
2018-04-16 15:42:01 +0800

    update ES6

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-var http = require("http");
-var fs = require("fs");
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req, res) => {
+    console.log('http start')
+}).listen(1121)
+let i = 0
 
-var server = http.createServer(function(req, res) {}).listen(1121);
-console.log("http start");
-
-
-    var i = 0;
-setInterval(function(){
-
-    i ++;
-    // console.log(i);
-    var url = "http://7xkmtv.com1.z0.glb.clouddn.com/" + i + ".jpg";
-    http.get(url, function(res) {
-        var imgData = "";
-
-        res.setEncoding("binary"); //一定要设置response的编码为binary否则会下载下来的图片打不开
-
-
-        res.on("data", function(chunk) {
-            imgData += chunk;
+setInterval(() => {
+    i++;
+    const url = `http://7xkmtv.com1.z0.glb.clouddn.com/${i}.jpg`
+    http.get(url, res => {
+        let imgData
+        res.setEncoding('binary') //一定要设置response的编码为binary否则会下载下来的图片打不开
+        res.on('data', chunk => {
+            imgData += chunk
         });
-
-        res.on("end", function() {
-            console.log(i);
-            fs.writeFile("./public/" + i + ".jpg", imgData, "binary", function(err) {
+        res.on('end', () => {
+            fs.writeFile(`./public/${i}.jpg`, imgData, 'binary', err => {
                 if (err) {
-                    console.log("down fail");
+                    console.log('down fail')
+                    return
                 }
-                console.log("down success");
-            });
-        });
-    });
-},1000)
-
+                console.log(`down ${i}.jpg success`)
+            })
+        })
+    })
+}, 1000)
