b7339ccfabef0ace50927ce9fdd12bfb536b6cc3
ihoey <mail@ihoey.com>
2018-06-26 17:25:05 +0800

    br

--- app.js
+++ app.js
@@ -1,27 +1,12 @@
-const http = require('http')
-const fs = require('fs')
-const server = http.createServer((req, res) => {
-    console.log('http start')
-}).listen(1121)
+const _exec = require('child_process').exec;
 let i = 0
 
 setInterval(() => {
     i++;
-    const url = `http://7xkmtv.com1.z0.glb.clouddn.com/${i}.jpg`
-    http.get(url, res => {
-        let imgData
-        res.setEncoding('binary') //一定要设置response的编码为binary否则会下载下来的图片打不开
-        res.on('data', chunk => {
-            imgData += chunk
-        });
-        res.on('end', () => {
-            fs.writeFile(`./public/${i}.jpg`, imgData, 'binary', err => {
-                if (err) {
-                    console.log('down fail')
-                    return
-                }
-                console.log(`down ${i}.jpg success`)
-            })
-        })
-    })
-}, 1000)
+    try {
+        // _exec(`wget -P ./public/ http://7xkmtv.com1.z0.glb.clouddn.com/${i}.jpg`);
+        _exec(`wget -P ./qq_product_operations/ https://sqimg.qq.com/qq_product_operations/tiqq/signin/pic2/v2/nd${i}.jpg`);
+    } catch (e) {
+        console.log(e);
+    }
+}, 100)
